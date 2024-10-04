b01766855bc52e263c71272289c0e2be7f6234de
arakashnawin <akashnawin18.ia@gmail.com>
2024-10-04 21:28:49 +0530

    Added modules folder

---
+++ modules/instances/variables.tf
@@ -0,0 +1,21 @@
+variable "amiid" {
+  default = "ami-08e5424edfe926b43"
+}
+
+variable "type" {
+  default = "t2.micro"
+}
+
+variable "pemfile" { 
+  default = "JanFeb24"
+}
+
+variable "volsize" {
+  type = number
+  default = 8
+}
+
+variable "servername" {
+  default = "demoserver"
+}
+
